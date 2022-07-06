4bc0942db027b215700952946140feafa0e420bd
Rincy Issac <rincy@allianz.com>
2022-07-06 15:40:02 +0200

    second commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='myinsuranceappscripts',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/sreejithtsnair/MyInsuranceAppScripts-CaseStudy.git',
+    author='SreeRincyUnax',
+    author_email='team1@example.com'
+)
