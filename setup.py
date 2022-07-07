8ea99fc26a5805ba57a9ae96637165a09177fbaa
Rincy Issac <rincy@allianz.com>
2022-07-07 17:13:11 +0200

    commit1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myinsuranceappscripts',
-    version='0.11',
+    version='0.8',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
