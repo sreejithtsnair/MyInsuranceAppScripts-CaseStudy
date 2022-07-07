bf49bbcb8d097f2d56623cd0def0d54f5437a2ac
Rincy Issac <rincy@allianz.com>
2022-07-07 08:27:39 +0200

    ignore commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myinsuranceappscripts',
-    version='0.1',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
