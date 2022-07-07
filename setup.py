dee245a28cb738d8efb7b3cae2aa469c5b78ca0b
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-07 10:22:53 +0200

    New update - merging version

--- setup.py
+++ setup.py
@@ -2,20 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(
-<<<<<<< HEAD
     name='myinsurancescripts',
     version='0.4',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package - updated',
-=======
-    name='myinsuranceappscripts',
-    version='0.12',
-    packages=find_packages(exclude=['tests*']),
-    license='MIT',
-    description='An example python package',
-    long_description=open('README.txt').read(),
->>>>>>> cff6fec81da4329b2b6bb6caa96e423d2e2ad504
     install_requires=['numpy'],
     url='https://github.com/sreejithtsnair/MyInsuranceAppScripts-CaseStudy.git',
     author='SreeRincyUnax',
